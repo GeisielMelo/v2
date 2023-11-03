0bee38bb0537ea6e1e4fbd04a2331f6d5f19d179
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-11-03 16:52:04 -0300

    test: Created two new sections for testing purposes.

---
+++ src/components/sections/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const About = () => {
+  return (
+    <section>
+      <div>
+        <p>Hero</p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/sections/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Hero = () => {
+  return (
+    <section>
+      <div>
+        <p>Hero</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;

d62e4545896d3d79411f9f41b8722bb4004e1354
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-11-05 13:34:14 -0300

    feat: added some content in About section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import styled from "styled-components";
+import { CodeBox } from "../CodeBox";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  h1 {
+    font-size: clamp(9px, 5vw, 24px);
+    font-family: ${(props) => props.theme.font.family.one};
+    color: ${(props) => props.theme.color.white.default};
+    border: 1px solid ${(props) => props.theme.color.grey.transparent};
+    background-color: transparent;
+    font-weight: 500;
+    margin-bottom: 30px;
+    padding: 5px 10px;
+    text-align: center;
+  }
+`;
 
 const About = () => {
   return (
     <section>
-      <div>
-        <p>Hero</p>
-      </div>
+      <Container>
+        <h1>{"Learn more <About> Me!"}</h1>
+        <CodeBox />
+      </Container>
     </section>
   );
 };

8b60b5c8a3be59e0ea540cd19a2c1e6807329b3d
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-11-06 14:07:38 -0300

    feat: updated component css

--- src/components/FeaturedProject.jsx
+++ src/components/FeaturedProject.jsx
@@ -1,48 +1,96 @@
 import React from "react";
 import styled from "styled-components";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LaunchIcon from "@mui/icons-material/Launch";
 import example from "../assets/img/example.png";
 
 const Content = styled.div`
-  float: ${(props) => props.float};
   position: relative;
   max-width: 700px;
+  height: 400px;
+  width: calc(100% - 20px);
+  float: ${(props) => props.float};
+
   display: flex;
-  color: #fff;
+  color: ${(props) => props.theme.color.white.default};
+  outline: 1px solid ${(props) => props.theme.color.grey.transparent};
+  border-radius: 12px;
   margin: 0px 10px 100px 10px;
+
+  background-image: url(${(props) => props.imageUrl});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: ${(props) => props.theme.color.black.transparent};
+    border-radius: 12px;
+  }
+
   &:last-of-type {
     margin-bottom: 0px;
   }
 `;
 
-const Image = styled.img`
-  width: 100%;
-`;
-
 const Description = styled.div`
+  margin: 20px 10px;
+  padding: 10px;
   position: absolute;
+  bottom: 0;
   max-width: 500px;
-  bottom: 100px;
   text-align: ${(props) => props.float};
+  background-color: ${(props) => props.theme.color.cyan.transparent};
+  border-radius: 6px;
   h1 {
     margin: 5px 0;
+    font-weight: 300;
+    font-family: ${(props) => props.theme.font.family.two};
+    font-size: ${(props) => props.theme.font.size.sm};
   }
   h2 {
     font-size: 20px;
+    font-family: ${(props) => props.theme.font.family.one};
   }
   p {
     margin: 20px 0;
+    border-radius: 6px;
+    font-family: ${(props) => props.theme.font.family.two};
+    font-size: ${(props) => props.theme.font.size.sm};
   }
   ul {
     display: flex;
     list-style-type: none;
+    justify-content: ${(props) => (props.float === "right" ? "flex-end" : "flex-start")};
+    font-family: ${(props) => props.theme.font.family.two};
+    font-size: ${(props) => props.theme.font.size.sm};
+    gap: 10px;
+    margin: 10px 0;
+    flex-wrap: wrap;
+  }
+  .buttons {
+    display: flex;
+    gap: 10px;
+    justify-content: ${(props) => (props.float === "right" ? "flex-end" : "flex-start")};
+    button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: none;
+      color: ${(props) => props.theme.color.white.default};
+    }
   }
 `;
 
 export const FeaturedProject = ({ float, title, subTitle, description, techList }) => {
   return (
     <>
-      <Content float={float}>
-        <Image src={example} alt="" />
+      <Content float={float} imageUrl={example}>
         <Description float={float} style={float === "left" ? { left: 0 } : { right: 0 }}>
           <div>
             <h1>Featured Project</h1>
@@ -53,12 +101,20 @@ export const FeaturedProject = ({ float, title, subTitle, description, techList 
               deserunt?
             </p>
             <ul>
-              <li>Styled Components</li>
-              <li>Styled Components</li>
-              <li>Styled Components</li>
-              <li>Styled Components</li>
-              <li>Styled Components</li>
+              <li>Node</li>
+              <li>Node</li>
+              <li>Node</li>
+              <li>Node</li>
+              <li>Node</li>
             </ul>
+            <div className="buttons ">
+              <button>
+                <GitHubIcon />
+              </button>
+              <button>
+                <LaunchIcon />
+              </button>
+            </div>
           </div>
         </Description>
       </Content>
--- src/components/sections/Featured.jsx
+++ src/components/sections/Featured.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 import { FeaturedProject } from "../FeaturedProject";
+
+const Container = styled.div`
+text-align: center;
+color: ${(props) => props.theme.color.white.default};
+margin: 0px 30px 95px 30px;
+  h1 {
+    font-family: ${(props) => props.theme.font.family.one};
+  }
+  p {
+    margin-top: 5px;
+    font-family: ${(props) => props.theme.font.family.two};
+  }
+`;
 
 const Featured = () => {
   return (
     <section>
+      <Container>
+        <h1>Some Things I’ve Built</h1>
+        <p>A catalog of significant personal projects I've undertaken in my spare time.</p>
+      </Container>
+
       <FeaturedProject float={"left"} />
       <FeaturedProject float={"right"} />
       <FeaturedProject float={"left"} />

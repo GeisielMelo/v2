4c031ceb08d45709664833c4f008b87a7a39ae6c
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-11-07 01:02:41 -0300

    feat: Passed data and integrated components together.

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from "react";
+import enUS from "../locales/en-US.json";
+import ptBR from "../locales/pt-BR.json";
 import Nav from "../components/Nav";
 import Wrapper from "../components/Wrapper";
 import Hero from "../components/sections/Hero";
 import About from "../components/sections/About";
 import Featured from "../components/sections/Featured";
+import Projects from "../components/sections/Projects";
+import Tech from "../components/sections/Tech";
 import VerticalBox from "../components/VerticalBox";
 import Footer from "../components/Footer";
 
 const Index = () => {
-  const [isMobile, setIsMobile] = useState(false);
-  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(enUS);
   const [language, setLanguage] = useState("enUS");
+  const [loading, setLoading] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -33,6 +38,7 @@ const Index = () => {
     const newLanguage = language === "enUS" ? "ptBR" : "enUS";
     localStorage.setItem("language", JSON.stringify(newLanguage));
     setLanguage(newLanguage);
+    language === "enUS" ? setData(enUS) : setData(ptBR);
   };
 
   return (
@@ -44,7 +50,9 @@ const Index = () => {
           <Nav isMobile={isMobile} />
           <Hero />
           <About />
+          <Tech data={data.Technologies} />
           <Featured />
+          <Projects data={data.Archive} />
           <VerticalBox translate={handleLanguage} isMobile={isMobile} />
           <Footer />
         </Wrapper>

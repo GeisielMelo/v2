b4823ebfcabd1a369d5a513b6fdfceecd726a80e
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-11-10 01:17:24 -0300

    feat: Implemented a navigation system using react-router-dom.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import GlobalStyles from "./styles/GlobalStyles";
-import Index from "./pages/Index";
+import AppRoutes from './AppRoutes';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Index />
+      <AppRoutes />
     </ThemeProvider>
   );
 }

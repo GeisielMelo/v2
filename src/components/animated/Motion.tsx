f44175adc9505d36b2231ecd29978edfeaf90f57
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-10 11:05:50 -0300

    refactor: Converted into TypeScript.

--- src/components/animated/Motion.tsx
+++ src/components/animated/Motion.tsx
@@ -1,31 +1,37 @@
-import React, { useEffect } from "react";
-import { motion, useAnimation } from "framer-motion";
+import { useEffect } from 'react'
+import { motion, useAnimation } from 'framer-motion'
 
-export const HideMotion = ({ children, visible, delay = 0.2 }) => {
-  const controls = useAnimation();
+type Motion = {
+  children: React.ReactNode
+  visible?: boolean
+  delay?: number
+}
+
+export const HideMotion: React.FC<Motion> = ({ children, visible, delay = 0.2 }) => {
+  const controls = useAnimation()
 
   useEffect(() => {
     if (visible) {
       controls.start({
         opacity: 1,
         transition: { duration: 0.5, delay: delay },
-      });
+      })
     } else {
       controls.start({
         opacity: 0,
         transition: { duration: 0.5, delay: delay },
-      });
+      })
     }
-  }, [visible, controls, delay]);
+  }, [visible, controls, delay])
 
   return (
     <motion.div initial={{ opacity: 0 }} animate={controls} exit={{ opacity: 0 }}>
       {children}
     </motion.div>
-  );
-};
+  )
+}
 
-export const OpacityMotion = ({ children, delay = 0 }) => {
+export const OpacityMotion: React.FC<Motion> = ({ children, delay = 0 }) => {
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -35,10 +41,10 @@ export const OpacityMotion = ({ children, delay = 0 }) => {
     >
       {children}
     </motion.div>
-  );
-};
+  )
+}
 
-export const UpMotion = ({ children, delay = 0 }) => {
+export const UpMotion: React.FC<Motion> = ({ children, delay = 0 }) => {
   return (
     <motion.div
       initial={{ y: 50, opacity: 0 }}
@@ -48,18 +54,18 @@ export const UpMotion = ({ children, delay = 0 }) => {
     >
       {children}
     </motion.div>
-  );
-};
+  )
+}
 
-export const TableTrUpMotion = ({ children, delay = 0 }) => {
+export const TableTrUpMotion: React.FC<Motion> = ({ children, delay = 0 }) => {
   return (
     <motion.tr
-    initial={{ y: 50, opacity: 0 }}
-    animate={{ y: 0, opacity: 1 }}
-    exit={{ y: -50, opacity: 0 }}
-    transition={{ duration: 0.5, delay: delay }}
+      initial={{ y: 50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: -50, opacity: 0 }}
+      transition={{ duration: 0.5, delay: delay }}
     >
       {children}
     </motion.tr>
-  );
-};
+  )
+}

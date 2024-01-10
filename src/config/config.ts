cc5bfd92162b3ad9e048c773a8e29b25918c58ae
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-10 11:09:05 -0300

    feat: Created a configuration file for user data.

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+export default {
+  url: {
+    github: 'https://github.com/GeisielMelo',
+    linkedIn: 'https://www.linkedin.com/in/geisiel/',
+  },
+  contact: { 
+    email: 'geisiel.nascimento@gmail.com',
+    cel: '71 98160-8087',
+  },
+}

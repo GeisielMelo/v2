e2a80ef5c14905573a3e5556a8ca07cf52598369
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-10 13:12:08 -0300

    feat: Created a new utils folder with a module containing handle functions.

---
+++ src/utils/handleFunctions.ts
@@ -0,0 +1,11 @@
+export const openNewTab = (url: string) => {
+  return window.open(url, '_blank')
+}
+
+export const mailTo = (email: string) => {
+  return (window.location.href = `mailto:${email}`)
+}
+
+export const toggleDarkMode = () => {
+  return document.documentElement.classList.toggle('dark')
+}

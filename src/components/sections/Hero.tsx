6c37f62681e969f145b6a91fb8a627b83d70f753
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-10 11:06:54 -0300

    feat: Finished Hero section refactoring.

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,25 +1,35 @@
+import { UpMotion } from '../animated/Motion'
+
 const Hero: React.FC = () => {
   return (
-    <section
-      id='#'
-      className='flex flex-col h-screen justify-center text-white max-w-5xl w-full p-12 border border-zinc-700 transition-all font-inter'
-    >
-      <p className='text-[rgb(2,190,199)] font-sans mb-2'>
-        Hi, my name is
-      </p>
-      <h1 className='text-[clamp(20px,8vw,80px)] leading-tight font-semibold tracking-tighter'>
-        Geisiel Melo.
-      </h1>
-      <h2 className='text-[clamp(12px,5vw,60px)] leading-none font-semibold tracking-tighter'>
-        I'm a Full Stack Developer
-      </h2>
-      <p className='text-[clamp(9px,5vw,16px)] mt-6 max-w-xl leading-tight'>
-        Software engineer with experience in web application development. I'm
-        currently seeking new opportunities to apply my skills and knowledge.
-        With a special focus on creating accessible and user-centric solutions,
-        I am committed to building products that stand out and provide amazing
-        experiences for users.
-      </p>
+    <section id='#' className='flex flex-col h-screen justify-center text-white max-w-5xl w-full p-12 border border-zinc-700 transition-all font-inter'>
+      <UpMotion>
+        <p className='text-[rgb(2,190,199)] font-sans mb-2'>
+          Hi, my name is
+        </p>
+      </UpMotion>
+
+      <UpMotion delay={0.2}>
+        <h1 className='text-[clamp(20px,8vw,80px)] leading-tight font-semibold tracking-tighter'>
+          Geisiel Melo.
+        </h1>
+      </UpMotion>
+
+      <UpMotion delay={0.4}>
+        <h2 className='text-[clamp(12px,5vw,60px)] leading-none font-semibold tracking-tighter'>
+          I'm a Full Stack Developer
+        </h2>
+      </UpMotion>
+
+      <UpMotion delay={0.6}>
+        <p className='text-[clamp(9px,5vw,16px)] mt-6 max-w-xl leading-tight text-zinc-600'>
+          Software engineer with experience in web application development. I'm
+          currently seeking new opportunities to apply my skills and knowledge.
+          With a special focus on creating accessible and user-centric
+          solutions, I am committed to building products that stand out and
+          provide amazing experiences for users.
+        </p>
+      </UpMotion>
     </section>
   )
 }

970f08b2b4e2f45f86087a9dc88d868006819af2
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-11 10:51:21 -0300

    refactor: Refactored projects component.

---
+++ src/components/sections/Projects.tsx
@@ -0,0 +1,10 @@
+const Projects: React.FC = () => {
+  return (
+    <section id='projects' className='flex flex-col items-center text-center max-w-5xl w-full px-12 py-28 border border-t-0 border-zinc-700 text-white font-inter'>
+      <h1 className="text-xl">Other Noteworthy Projects</h1>
+      <p className="text-cyan-400 font-inter mt-2">Take a look!</p>
+    </section>
+  )
+}
+
+export default Projects

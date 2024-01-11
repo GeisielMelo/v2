93f14938a9dd2c7f0978c5812422e426c49d423c
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-11 16:07:05 -0300

    feat: Putting everything together in the index.tsx.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,15 +4,18 @@ import About from '../components/sections/About'
 import Contact from '../components/sections/Contact'
 import Projects from '../components/sections/Projects'
 import Featured from '../components/sections/Featured'
+import { useGithubData } from '../context/GithubDataContext'
 
 const Index: React.FC = () => {
+  const { data } = useGithubData()
+
   return (
     <>
       <main className='flex flex-col items-center px-4 sm:px-8 lg:px-24'>
         <Hero />
         <About />
-        <Featured/>
-        <Projects />
+        <Featured />
+        <Projects data={data}/>
         <Contact />
       </main>
       <Footer />

e31ece9923bff11971c7d783110f7ed8fbb637bf
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-10 23:31:39 -0300

    feat: disabled  archive page.

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Index from './pages/Index'
-import Archive from './pages/Archive'
+// import Archive from './pages/Archive'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='*' element={<Index />} />
-        <Route path='/archive' element={<Archive />} />
+        {/* <Route path='/archive' element={<Archive />} /> */}
         <Route path='/' element={<Index />} />
       </Routes>
     </BrowserRouter>

4475a8f2651f958cc60c8c0732c55540ea2245bc
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-11 10:53:44 -0300

    feat: Putting everything together in the index.tsx.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,19 @@
 import Hero from '../components/sections/Hero'
 import Footer from '../components/sections/Footer'
 import About from '../components/sections/About'
+import Contact from '../components/sections/Contact'
+import Projects from '../components/sections/Projects'
+import Featured from '../components/sections/Featured'
 
 const Index: React.FC = () => {
   return (
     <>
       <main className='flex flex-col items-center px-4 sm:px-8 lg:px-24'>
         <Hero />
-        <About/>
+        <About />
+        <Featured/>
+        <Projects />
+        <Contact />
       </main>
       <Footer />
     </>

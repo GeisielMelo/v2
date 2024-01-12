7e578c4a527715d83d923d77e04411085630d057
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-12 17:00:07 -0300

    feat: removed archive page

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
 import Index from './pages/Index'
-import Archive from './pages/Archive'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='*' element={<Index />} />
-        <Route path='/archive' element={<Archive />} />
-        <Route path='/' element={<Index />} />
       </Routes>
     </BrowserRouter>
   )

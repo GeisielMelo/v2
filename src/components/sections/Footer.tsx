6020785a174413f4e63a98368e3b48be1b839827
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2024-01-12 10:07:47 -0300

    feat: Implemented dark mode functionality on the button.

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,27 +1,43 @@
-import { GitHub, LinkedIn, WhatsApp, LightMode } from '@mui/icons-material'
+import { GitHub, LinkedIn, WhatsApp, LightMode, DarkMode } from '@mui/icons-material'
 import { openNewTab } from '../../utils/handleFunctions'
 import config from '../../config/config'
+import { useDarkMode } from '../../hooks/useDarkMode'
 
 const Footer: React.FC = () => {
-  const styledButtons = 'ease-in-out duration-300 p-2 border border-transparent hover:text-cyan-400 hover:border-cyan-400 hover:bg-zinc-700'
+  const { isDarkMode, setDarkMode } = useDarkMode()
+
+  const styledButtons =
+    'ease-in-out duration-300 p-2 border border-transparent hover:text-cyan-400 hover:border-cyan-400 hover:bg-zinc-700'
 
   return (
     <footer className='text-white py-4 font-inter text-sm'>
       <div className='flex items-center justify-center p-4 gap-4'>
         <div className='flex gap-4 lg:fixed bottom-0 left-8 lg:flex-col lg:items-center'>
-          <button className={styledButtons} onClick={() => openNewTab(config.url.github)}>
+          <button
+            className={styledButtons}
+            onClick={() => openNewTab(config.url.github)}
+          >
             <GitHub />
           </button>
-          <button className={styledButtons} onClick={() => openNewTab(config.url.linkedIn)}>
+          <button
+            className={styledButtons}
+            onClick={() => openNewTab(config.url.linkedIn)}
+          >
             <LinkedIn />
           </button>
           <div className='hidden h-36 w-[0.125rem] bg-white lg:block' />
         </div>
         <div className='flex gap-4 lg:fixed bottom-0 right-8 lg:flex-col lg:items-center'>
-          <button className={styledButtons}>
-            <LightMode />
+          <button
+            className={styledButtons}
+            onClick={() => setDarkMode(!isDarkMode)}
+          >
+            {isDarkMode ? <LightMode /> : <DarkMode/>}
           </button>
-          <button className={styledButtons} onClick={() => openNewTab(config.url.whatsApp)}>
+          <button
+            className={styledButtons}
+            onClick={() => openNewTab(config.url.whatsApp)}
+          >
             <WhatsApp />
           </button>
           <div className='hidden h-36 w-[0.125rem] bg-white lg:block' />
@@ -29,7 +45,10 @@ const Footer: React.FC = () => {
       </div>
 
       <div className='w-full flex justify-center py-4'>
-        <p className='max-w-max px-2 text-zinc-700 ease-in-out duration-300 hover:cursor-pointer hover:text-cyan-400' onClick={() => openNewTab(config.url.github)}>
+        <p
+          className='max-w-max px-2 text-zinc-700 ease-in-out duration-300 hover:cursor-pointer hover:text-cyan-400'
+          onClick={() => openNewTab(config.url.github)}
+        >
           © Designed & Built by {config.contact.git}.
         </p>
       </div>
